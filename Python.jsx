11d174ca53dfe51878844857a45d0ccd9d0a81fd
SHOWMINI <84834137+SHOWMINI@users.noreply.github.com>
2021-06-27 22:12:03 +0530

    Update Python.jsx

--- Python.jsx
+++ Python.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Python() {
   return (
     <div class="note">
-       <h1>Python</h1>
+       <h1>PYTHON</h1>
        <p>Here we bring our another free live BOOTCAMP with a hands-on project at the end - Foundation of python and Natural Progarmming Processing</p>
     </div>
   );
