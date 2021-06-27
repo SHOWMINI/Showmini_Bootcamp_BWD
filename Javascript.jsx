ef5c0a415b20f11a67a9908974b7d8290916e13a
SHOWMINI <84834137+SHOWMINI@users.noreply.github.com>
2021-06-27 22:08:17 +0530

    Update Javascript.jsx

--- Javascript.jsx
+++ Javascript.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Javascript() {
   return (
     <div class="note">
-       <h1>Python</h1>
+       <h1>Javascript</h1>
        <p>In September 1995, a Netscape programmer named Brandan Eich developed a new scripting language in just 10 days. It was originally named Mocha, but quickly became known as LiveScript and, later, JavaScript.</p>
     </div>
   );
